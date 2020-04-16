eb3c4ce333a1e4ea19665b40122c27d72b37bd0e
Danny Waite <dannywaite7@gmail.com>
2020-04-16 14:16:59 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        method="post"
+        action="https://emailoctopus.com/lists/d2bf5d5b-7f30-11ea-a3d0-06b4694bee2a/members/embedded/1.3s/add"
+        data-sitekey="6LdYsmsUAAAAAPXVTt-ovRsPIJ_IVhvYBBhGvRV6"
+      >
+        <input name="field_0" id="field_0" type="email" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
